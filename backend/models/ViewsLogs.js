3335de0212c3861f45409551c1550be184deeb5b
Astralapse <vladlubcenko8@gmail.com>
2024-06-28 23:16:35 +0300

    fixed

--- backend/models/ViewsLogs.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const ViewsLogsSchema = new mongoose.Schema({
-  log: {
-    type: String,
-  },
-});
-
-export default mongoose.model("ViewsLogs", ViewsLogsSchema);
